56aa4757899ee5fc282c509a95218ee4b4f2214e
Ivan Kozlov <69389177+IYunet@users.noreply.github.com>
2023-04-05 20:27:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,10 +7,12 @@ from scipy.stats import chi2
 
 chat_id = 426527714 # Ваш chat ID, не меняйте название переменной
 
-def solution(p: float, x: np.array) -> tuple:    
-    s = 44
+def solution(p: float, x: np.array) -> tuple:
+    alpha = 1 - p
     n = x.shape[0]
-    alpha = 1 - p    
-    scale = np.var(x)
-    return np.sqrt(scale / chi2.ppf(1 - alpha / 2, df=n-1) / s), \
-           np.sqrt(scale / chi2.ppf(alpha / 2, df=n-1) / s)
+    s2 = np.var(x, ddof=1)
+    quant1 = chi2.ppf(1-alpha/2, n-1)
+    quant2 = chi2.ppf(alpha/2, n-1)
+    left = np.sqrt((n-1)*s2/(44*quant1))
+    right = np.sqrt((n-1)*s2/(44*quant2))
+    return (left, right)
