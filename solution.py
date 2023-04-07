3f698dcebb45700fb1b2fb5e09d2b862137d850f
Ivan Kozlov <69389177+IYunet@users.noreply.github.com>
2023-04-07 16:47:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,14 +3,16 @@ import numpy as np
 
 from scipy.stats import norm
 from scipy.stats import chi2
-
+import scipy.stats as t
 
 chat_id = 426527714 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
-    n = x.size
-    sum_x_2 = sum(x**2)
-    q_l = chi2(2 * n).ppf(q=alpha / 2)
-    q_r = chi2(2 * n).ppf(q=1 - alpha / 2)
-    return np.sqrt(sum_x_2 / q_r / 44), np.sqrt(sum_x_2 / q_l / 44)
+    n = x.shape[0]
+    s = ( np.sum((x - x.mean())**2) / (n - 1) )**0.5
+    quant1 = chi2.ppf(1-alpha/2, 2*n)
+    quant2 = chi2.ppf(alpha/2, 2*n)
+    left = s * np.sqrt((n - 1)  / (quant1*44) )
+    right = s * np.sqrt((n - 1) / (quant2*44) )
+    return (left,right)
